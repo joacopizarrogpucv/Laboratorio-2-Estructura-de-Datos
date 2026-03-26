85f15ae591d3231dd12a9d24bbd75e5674cd8ed4
Your Name <you@example.com>
2026-03-26 19:57:58 +0000

    new try

--- list.c
+++ list.c
@@ -75,7 +75,12 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-    return NULL;
+    if (list->current == NULL || list->current->prev == NULL){
+        return NULL;
+    }
+    void * dato2 = list->current->prev->data;
+    list->current = list->current->prev;
+    return dato2;
 }
 
 // 4. Programe la función void pushFront(List * list, void * data), la cual agrega un dato al comienzo de la lista.
